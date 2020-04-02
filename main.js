52e65c6bdaba0838cde10eb27409b0bdc4a3a749
lavaboy1738 <yanzfeng@bu.edu>
2020-04-02 17:59:45 -0400

    adding random word feature to the button

--- main.js
+++ main.js
@@ -5,6 +5,26 @@ btn.addEventListener("mouseover", function(){
     const height = Math.floor(Math.random()*(window.innerHeight - 70))
     btn.style.top = `${height}px`
     btn.style.left = `${width}px`
+    switch(Math.floor(Math.random()*7)){
+        case 0:
+            btn.innerText = "Close!";
+            break;
+        case 1:
+            btn.innerText = "Almost!";
+            break;
+        case 3:
+            btn.innerText = "Phew!";
+            break;
+        case 4:
+            btn.innerText = "Nice Try~";
+            break;
+        case 5:
+            btn.innerText = "Really?";
+            break;
+        case 6:
+            btn.innerText = "Nope.";
+            break;
+    }
 })
 
 btn.addEventListener("click", function(){
